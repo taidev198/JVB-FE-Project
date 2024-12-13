2b4e65cc11a52f31a1403f7559349a574ccda425
viet <lanviethoang2004@gmail.com>
2024-12-13 17:21:03 +0700

    fig ui logic register login

--- components/auth/Company/RegisterCompanyComponent.tsx
+++ components/auth/Company/RegisterCompanyComponent.tsx
@@ -82,228 +82,230 @@ const RegisterCompanyComponent = () => {
     dispatch(setLoading(isLoadingDistricts || isLoadingProvinces || isLoadingWard || isLoadingRegister));
   }, [dispatch, isLoadingDistricts, isLoadingProvinces, isLoadingWard, isLoadingRegister, data?.success, isSuccess]);
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="w-full px-5 sm:px-0">
-      <h1 className="my-10 text-2xl font-bold">Đăng ký tài khoản Doanh nghiệp</h1>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        {/* Name school */}
-        <>
-          <Input
-            name="companyName"
-            label="Tên doanh nghiệp"
-            placeholder="Nhập tên doanh nghiệp"
-            control={control}
-            error={errors.companyName?.message}
-            required={true}
-          />
-        </>
-        {/* Code school */}
-        <>
-          <Input
-            name="companyCode"
-            label="Mã doanh nghiệp"
-            placeholder="Nhập mã doanh nghiệp"
-            control={control}
-            error={errors.companyCode?.message}
-            required={true}
-          />
-        </>
-        {/* Code school */}
-        <>
-          <Input name="taxCode" label="Mã số thuế" placeholder="Nhập mã số thuế" control={control} error={errors.taxCode?.message} required={true} />
-        </>
-        {/* Email */}
-        <>
-          <Input name="email" label="Email" placeholder="Nhập Email" control={control} error={errors.email?.message} required={true} />
-        </>
-        {/* Password */}
-        <>
-          <Input
-            type="password"
-            name="password"
-            label="Mật khẩu"
-            placeholder="Nhập Mật khẩu"
-            control={control}
-            error={errors.password?.message}
-            required={true}
-          />
-        </>
-        {/*Confirm Password */}
-        <>
-          <Input
-            type="password"
-            name="confirm_password"
-            label="Xác nhận mật khẩu"
-            placeholder="Xác nhận mật khẩu"
-            control={control}
-            error={errors.confirm_password?.message}
-            required={true}
-          />
-        </>
-        {/* Phone */}
-        <>
-          <Input
-            type="text"
-            name="phoneNumber"
-            label="Số điện thoại"
-            placeholder="Nhập số điện thoại"
-            control={control}
-            error={errors.phoneNumber?.message}
-            required={true}
-          />
-        </>
-        {/* Establihed date */}
-        <>
-          <Input
-            type="date"
-            name="establishDate"
-            label="Ngày thành lập"
-            placeholder=""
-            control={control}
-            error={errors.establishDate?.message}
-            required={true}
-          />
-        </>
-      </div>
-      {/* Địa chỉ */}
-      <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <div>
-          <label htmlFor="provinceId" className="mb-1 block text-sm font-semibold text-gray-700">
-            Tỉnh <span className="text-red-600">*</span>
-          </label>
-          <Controller
-            name="provinceId"
-            control={control}
-            render={({ field }) => (
-              <Select
-                {...field}
-                className="basic-single"
-                classNamePrefix="select"
-                placeholder="Chọn Tỉnh/Thành phố"
-                isLoading={isLoadingProvinces}
-                options={provinces?.data || []}
-                getOptionLabel={(option: { provinceName: any }) => option.provinceName || ''} // Hiển thị tên tỉnh
-                getOptionValue={(option: { id: any }) => option.id} // Chỉ lưu id
-                onChange={(selectedOption: { id: any }) => {
-                  field.onChange(selectedOption ? selectedOption.id : null); // Lưu id vào form
-                }}
-                value={provinces?.data?.find(option => option.id === field.value)} // Giữ giá trị name (tên tỉnh) khi chọn
-                ref={field.ref}
-                styles={{
-                  placeholder: (provided: any) => ({
-                    ...provided,
-                    fontSize: '14px',
-                  }),
-                }}
-              />
-            )}
-          />
-          {errors.provinceId && <p className="mt-2 text-sm text-red-500">{errors.provinceId.message}</p>}
+    <>
+      <h1 className="my-6 px-6 text-xl font-bold md:mx-5 md:px-0 md:text-2xl">Đăng ký tài khoản doanh nghiệp</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full rounded-md border p-6 shadow">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          {/* Name school */}
+          <>
+            <Input
+              name="companyName"
+              label="Tên doanh nghiệp"
+              placeholder="Nhập tên doanh nghiệp"
+              control={control}
+              error={errors.companyName?.message}
+              required={true}
+            />
+          </>
+          {/* Code school */}
+          <>
+            <Input
+              name="companyCode"
+              label="Mã doanh nghiệp"
+              placeholder="Nhập mã doanh nghiệp"
+              control={control}
+              error={errors.companyCode?.message}
+              required={true}
+            />
+          </>
+          {/* Code school */}
+          <>
+            <Input name="taxCode" label="Mã số thuế" placeholder="Nhập mã số thuế" control={control} error={errors.taxCode?.message} required={true} />
+          </>
+          {/* Email */}
+          <>
+            <Input name="email" label="Email" placeholder="Nhập Email" control={control} error={errors.email?.message} required={true} />
+          </>
+          {/* Password */}
+          <>
+            <Input
+              type="password"
+              name="password"
+              label="Mật khẩu"
+              placeholder="Nhập Mật khẩu"
+              control={control}
+              error={errors.password?.message}
+              required={true}
+            />
+          </>
+          {/*Confirm Password */}
+          <>
+            <Input
+              type="password"
+              name="confirm_password"
+              label="Xác nhận mật khẩu"
+              placeholder="Xác nhận mật khẩu"
+              control={control}
+              error={errors.confirm_password?.message}
+              required={true}
+            />
+          </>
+          {/* Phone */}
+          <>
+            <Input
+              type="text"
+              name="phoneNumber"
+              label="Số điện thoại"
+              placeholder="Nhập số điện thoại"
+              control={control}
+              error={errors.phoneNumber?.message}
+              required={true}
+            />
+          </>
+          {/* Establihed date */}
+          <>
+            <Input
+              type="date"
+              name="establishDate"
+              label="Ngày thành lập"
+              placeholder=""
+              control={control}
+              error={errors.establishDate?.message}
+              required={true}
+            />
+          </>
         </div>
+        {/* Địa chỉ */}
+        <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <div>
+            <label htmlFor="provinceId" className="mb-1 block text-sm font-semibold text-gray-700">
+              Tỉnh <span className="text-red-600">*</span>
+            </label>
+            <Controller
+              name="provinceId"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  className="basic-single"
+                  classNamePrefix="select"
+                  placeholder="Chọn Tỉnh/Thành phố"
+                  isLoading={isLoadingProvinces}
+                  options={provinces?.data || []}
+                  getOptionLabel={(option: { provinceName: any }) => option.provinceName || ''} // Hiển thị tên tỉnh
+                  getOptionValue={(option: { id: any }) => option.id} // Chỉ lưu id
+                  onChange={(selectedOption: { id: any }) => {
+                    field.onChange(selectedOption ? selectedOption.id : null); // Lưu id vào form
+                  }}
+                  value={provinces?.data?.find(option => option.id === field.value)} // Giữ giá trị name (tên tỉnh) khi chọn
+                  ref={field.ref}
+                  styles={{
+                    placeholder: (provided: any) => ({
+                      ...provided,
+                      fontSize: '14px',
+                    }),
+                  }}
+                />
+              )}
+            />
+            {errors.provinceId && <p className="mt-2 text-sm text-red-500">{errors.provinceId.message}</p>}
+          </div>
 
-        {/* Chọn Huyện */}
-        <div>
-          <label htmlFor="districtId" className="mb-1 block text-sm font-semibold text-gray-700">
-            Huyện <span className="text-red-600">*</span>
-          </label>
-          <Controller
-            name="districtId"
-            control={control}
-            render={({ field }) => (
-              <Select
-                {...field}
-                className="basic-single"
-                classNamePrefix="select"
-                placeholder="Chọn Quận/Huyện"
-                isLoading={isLoadingDistricts}
-                options={districts?.data || []}
-                getOptionLabel={(option: { districtName: any }) => option.districtName || ''} // Hiển thị tên tỉnh
-                getOptionValue={(option: { id: any }) => option.id} // Chỉ lưu id
-                onChange={(selectedOption: { id: any }) => {
-                  field.onChange(selectedOption ? selectedOption.id : null); // Lưu id vào form
-                }}
-                value={districts?.data?.find(option => option.id === field.value)} // Giữ giá trị name (tên tỉnh) khi chọn
-                ref={field.ref}
-                styles={{
-                  placeholder: (provided: any) => ({
-                    ...provided,
-                    fontSize: '14px',
-                  }),
-                }}
-              />
-            )}
-          />
-          {errors.districtId && <p className="mt-2 text-sm text-red-500">{errors.districtId.message}</p>}
-        </div>
+          {/* Chọn Huyện */}
+          <div>
+            <label htmlFor="districtId" className="mb-1 block text-sm font-semibold text-gray-700">
+              Huyện <span className="text-red-600">*</span>
+            </label>
+            <Controller
+              name="districtId"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  className="basic-single"
+                  classNamePrefix="select"
+                  placeholder="Chọn Quận/Huyện"
+                  isLoading={isLoadingDistricts}
+                  options={districts?.data || []}
+                  getOptionLabel={(option: { districtName: any }) => option.districtName || ''} // Hiển thị tên tỉnh
+                  getOptionValue={(option: { id: any }) => option.id} // Chỉ lưu id
+                  onChange={(selectedOption: { id: any }) => {
+                    field.onChange(selectedOption ? selectedOption.id : null); // Lưu id vào form
+                  }}
+                  value={districts?.data?.find(option => option.id === field.value)} // Giữ giá trị name (tên tỉnh) khi chọn
+                  ref={field.ref}
+                  styles={{
+                    placeholder: (provided: any) => ({
+                      ...provided,
+                      fontSize: '14px',
+                    }),
+                  }}
+                />
+              )}
+            />
+            {errors.districtId && <p className="mt-2 text-sm text-red-500">{errors.districtId.message}</p>}
+          </div>
 
-        {/* Chọn Xã */}
-        <div>
-          <label htmlFor="wardId" className="mb-1 block text-sm font-semibold text-gray-700">
-            Xã <span className="text-red-600">*</span>
-          </label>
-          <Controller
-            name="wardId"
-            control={control}
-            render={({ field }) => (
-              <Select
-                {...field}
-                className="basic-single"
-                classNamePrefix="select"
-                placeholder="Chọn Xã/Phường"
-                isLoading={isLoadingWard}
-                options={wards?.data || []}
-                getOptionLabel={(option: { wardName: any }) => option.wardName || ''} // Hiển thị tên tỉnh
-                getOptionValue={(option: { id: any }) => option.id} // Chỉ lưu id
-                onChange={(selectedOption: { id: any }) => {
-                  field.onChange(selectedOption ? selectedOption.id : null); // Lưu id vào form
-                }}
-                value={wards?.data?.find(option => option.id === field.value)} // Giữ giá trị name (tên tỉnh) khi chọn
-                ref={field.ref}
-                styles={{
-                  placeholder: (provided: any) => ({
-                    ...provided,
-                    fontSize: '14px',
-                  }),
-                }}
-              />
-            )}
-          />
-          {errors.wardId && <p className="mt-2 text-sm text-red-500">{errors.wardId.message}</p>}
+          {/* Chọn Xã */}
+          <div>
+            <label htmlFor="wardId" className="mb-1 block text-sm font-semibold text-gray-700">
+              Xã <span className="text-red-600">*</span>
+            </label>
+            <Controller
+              name="wardId"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  className="basic-single"
+                  classNamePrefix="select"
+                  placeholder="Chọn Xã/Phường"
+                  isLoading={isLoadingWard}
+                  options={wards?.data || []}
+                  getOptionLabel={(option: { wardName: any }) => option.wardName || ''} // Hiển thị tên tỉnh
+                  getOptionValue={(option: { id: any }) => option.id} // Chỉ lưu id
+                  onChange={(selectedOption: { id: any }) => {
+                    field.onChange(selectedOption ? selectedOption.id : null); // Lưu id vào form
+                  }}
+                  value={wards?.data?.find(option => option.id === field.value)} // Giữ giá trị name (tên tỉnh) khi chọn
+                  ref={field.ref}
+                  styles={{
+                    placeholder: (provided: any) => ({
+                      ...provided,
+                      fontSize: '14px',
+                    }),
+                  }}
+                />
+              )}
+            />
+            {errors.wardId && <p className="mt-2 text-sm text-red-500">{errors.wardId.message}</p>}
+          </div>
+          <div>
+            <Input
+              type="text"
+              name="houseNumber"
+              label="Số nhà, đường"
+              placeholder="Nhập số nhà, đường"
+              control={control}
+              error={errors.houseNumber?.message}
+              required={true}
+            />
+          </div>
         </div>
-        <div>
-          <Input
-            type="text"
-            name="houseNumber"
-            label="Số nhà, đường"
-            placeholder="Nhập số nhà, đường"
-            control={control}
-            error={errors.houseNumber?.message}
-            required={true}
-          />
+        {/* Number house */}
+        <div className="mt-4 flex flex-col">
+          {/* Description */}
+          <>
+            <Controller
+              name="companyDescription"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <TextEditor
+                  value={field.value}
+                  onChange={field.onChange}
+                  onBlur={field.onBlur}
+                  label="Mô tả chi tiết"
+                  error={errors.companyDescription?.message}
+                  required={true}
+                />
+              )}
+            />
+          </>
         </div>
-      </div>
-      {/* Number house */}
-      <div className="mt-4 flex flex-col">
-        {/* Description */}
-        <>
-          <Controller
-            name="companyDescription"
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <TextEditor
-                value={field.value}
-                onChange={field.onChange}
-                onBlur={field.onBlur}
-                label="Mô tả chi tiết"
-                error={errors.companyDescription?.message}
-                required={true}
-              />
-            )}
-          />
-        </>
-      </div>
-      <Button text="Đăng ký" full={true} type="submit" className="mt-5" />
-    </form>
+        <Button text="Đăng ký" full={true} type="submit" className="mt-5" />
+      </form>
+    </>
   );
 };
 export default RegisterCompanyComponent;
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -4,6 +4,9 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useRouter } from 'next/router';
+import { Checkbox } from '@mui/material';
+import toast from 'react-hot-toast';
+import Link from 'next/link';
 
 import { Button } from '@/components/Common/Button';
 import Input from '@/components/Common/Input';
@@ -12,7 +15,6 @@ import { useLoginMutation } from '@/services/adminSystemApi';
 import { useAppDispatch } from '@/store/hooks';
 import { logIn } from '@/store/slices/user';
 import { setLoading } from '@/store/slices/global';
-import { setToast } from '@/store/slices/toastSlice';
 import { roles } from '@/utils/app/const';
 
 interface FormDataLogin {
@@ -69,7 +71,7 @@ const Login = () => {
     if (isError) {
       if ('status' in error) {
         const errMsg = 'error' in error ? error.error : JSON.stringify(error.data.message);
-        dispatch(setToast({ message: errMsg, type: 'error' }));
+        toast.error(errMsg);
       }
     }
   }, [isSuccess, isError, data?.data]);
@@ -77,12 +79,11 @@ const Login = () => {
   return (
     <AuthLayout type="login">
       <form onSubmit={handleSubmit(onSubmit)} className="w-[300px]">
-        <h1 className="my-10 text-2xl font-bold">Đăng nhập</h1>
-        <div className="flex flex-col gap-4">
-          <>
+        <h1 className="my-6 text-2xl font-bold">Đăng nhập</h1>
+        <div className="border p-5 shadow">
+          <div className="flex flex-col gap-4 rounded-lg ">
             <Input name="email" label="Email" placeholder="Nhập email" control={control} error={errors.email?.message} required={true} />
-          </>
-          <>
+
             <Input
               type="password"
               name="password"
@@ -92,8 +93,21 @@ const Login = () => {
               error={errors.password?.message}
               required={true}
             />
-          </>
-          <Button text="Đăng nhập" full={true} type="submit" />
+          </div>
+          <div className="mt-2 flex flex-col gap-3">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <Checkbox id="remember-password" color="success" size="small" className="!p-0" />
+                <label htmlFor="remember-password" className="cursor-pointer text-sm">
+                  Nhớ mật khẩu
+                </label>
+              </div>
+              <Link href={'/auth/forgot-password/send-email'} className="text-sm text-primary-main">
+                Quên mật khẩu ?
+              </Link>
+            </div>
+            <Button text="Đăng nhập" full={true} type="submit" />
+          </div>
         </div>
       </form>
     </AuthLayout>
