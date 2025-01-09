982ab7be78452776a59deadb36ba6f1c22fbd9c1
huongph44799 <lanhuong26072004@gmail.com>
2025-01-09 15:30:44 +0700

    Fixbug department

--- components/Common/ButtonIcon/ArrowDownwardIcon.tsx
+++ components/Common/ButtonIcon/ArrowDownwardIcon.tsx
@@ -1,10 +1,10 @@
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { Tooltip } from '@mui/material';
 const ButtonArrow = () => {
   return (
     <Tooltip title="Xuống">
       <div className="cursor-pointer rounded-lg py-[6px] transition-all hover:bg-[#80807f1a]">
-        <ArrowDownwardIcon style={{ fontSize: 12 }} color="inherit" />
+        <ArrowDropDownIcon style={{ fontSize: 12 }} color="inherit" />
       </div>
     </Tooltip>
   );
--- components/Common/ButtonIcon/ArrowUpwardIcon.tsx
+++ components/Common/ButtonIcon/ArrowUpwardIcon.tsx
@@ -1,10 +1,10 @@
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import { Tooltip } from '@mui/material';
 const ButtonUp = () => {
   return (
     <Tooltip title="Lên">
       <div className="cursor-pointer rounded-lg py-[6px] transition-all hover:bg-[#80807f1a]">
-        <ArrowUpwardIcon style={{ fontSize: 12 }} color="inherit" />
+        <ArrowDropUpIcon style={{ fontSize: 12 }} color="inherit" />
       </div>
     </Tooltip>
   );
