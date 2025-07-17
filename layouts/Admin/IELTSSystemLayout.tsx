36868ffd2eff562b09d8d4d6364f5a723fd9ac8c
@taint <taint@nal.vn>
2025-07-17 14:42:53 +0700

    feat: create ielts admin page

---
+++ layouts/Admin/IELTSSystemLayout.tsx
@@ -0,0 +1,45 @@
+import React, { memo, ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useMediaQuery, useTheme } from '@mui/material';
+import Navbar from './Sidebar';
+import Header from '@/components/Header/Header';
+import { litsNavbarIELTSSystemAdmin } from '@/router/admin/navbarIELTSSystemRouter';
+import { useAppSelector } from '@/store/hooks';
+import { Loading } from '@/components/Common/Loading';
+
+const IELTSSystemLayout = memo(({ children }: { children: ReactNode }) => {
+  const theme = useTheme();
+  const isMobileAndTablet = useMediaQuery(theme.breakpoints.down('lg'));
+  const isLoading = useAppSelector(state => state.global.isLoading);
+  const router = useRouter();
+  const roleAccount = useAppSelector(state => state.user.roleAccount);
+  useEffect(() => {
+    // if (roleAccount !== 'ADMIN') {
+    //   router.push('/auth/login');
+    // }
+  }, [roleAccount, router]);
+  return (
+    <>
+      {/* Loading Overlay */}
+      {isLoading && <Loading />}
+
+      {/* Header */}
+      <Header 
+      // isAdmin={true}
+       />
+
+      {/* Main Layout */}
+      <div className="flex h-screen pt-[100px]">
+        {/* Sidebar */}
+        <div className={`${isMobileAndTablet ? 'hidden' : 'block'} h-full w-[19%] overflow-auto bg-primary-white p-[20px]`}>
+          <Navbar props={litsNavbarIELTSSystemAdmin} />
+        </div>
+
+        {/* Main Content */}
+        <div className="h-full w-full overflow-auto bg-[#EEEE] p-0 md:p-[20px] xl:w-[81%] xl:p-[20px]">{children}</div>
+      </div>
+    </>
+  );
+});
+
+export default IELTSSystemLayout;
